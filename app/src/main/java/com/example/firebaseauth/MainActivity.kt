e7c386233e62d307ac4b2d2678af15dfd054966f
MrZardari <sagheerhzardari@gmail.com>
2019-09-08 10:07:03 +0500

    Create Account Complete

--- app/src/main/java/com/example/firebaseauth/MainActivity.kt
+++ app/src/main/java/com/example/firebaseauth/MainActivity.kt
@@ -1,12 +1,47 @@
 package com.example.firebaseauth
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    //First We Need to Have a FirebaseAuth Instance
+    val mAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun createAccount(view: View) {
+        showToastS("Please Wait... Creating Account")
+        mAuth.createUserWithEmailAndPassword("sagheerhzardari@gmail.com", "12345678")
+            .addOnCompleteListener { task ->
+
+                if (task.isSuccessful)
+                    showToast("Created Your Account\nEmail: sagheerhzardari@gmail.com\nPass: 12345678")
+                else
+                    showToast(task.exception.toString())
+
+            }
+
+    }
+
+    fun logIn(view: View) {}
+    fun logOut(view: View) {}
+    fun verifyEmail(view: View) {}
+    fun forgotPassword(view: View) {}
+
+    fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    }
+
+    fun showToastS(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+
 }
